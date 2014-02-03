9969a1cfb84165216711e5b5bcac91aac142f495
Akeda Bagus <akeda@x-team.com>
2014-02-03 16:55:03 +0700

    Check with isset on options property.

--- lib/Janrain/HttpClient/Listener/AuthListener.php
+++ lib/Janrain/HttpClient/Listener/AuthListener.php
@@ -24,12 +24,12 @@ class AuthListener
 
 		// Use either 'access_token' or 'client_id' paired with 'client_secret'.
 		// Prioritize 'access_token' over 'client_id' and 'client_secret'.
-		if ($this->options['access_token']) {
+		if (isset($this->options['access_token']) && $this->options['access_token']) {
 			$parameters['access_token'] = $this->options['access_token'];
 
 			// Some endpoints expect 'token' instead of 'access_token'.
 			$parameters['token'] = $this->options['access_token'];
-		} else if ($this->options['client_id'] && $this->options['client_secret']) {
+		} else if (isset($this->options['client_id']) && $this->options['client_id'] && isset($this->options['client_secret']) && $this->options['client_secret']) {
 			$parameters['client_id']     = $this->options['client_id'];
 			$parameters['client_secret'] = $this->options['client_secret'];
 		} else {
@@ -37,13 +37,13 @@ class AuthListener
 		}
 
 		// Most Engage API calls need 'apiKey'.
-		if ($this->options['api_key']) {
+		if (isset($this->options['api_key']) && $this->options['api_key']) {
 			$parameters['apiKey'] = $this->options['api_key'];
 		}
 
 		// Most Partner API calls need 'partnerKey'.
-		if ($this->options['partner_key']) {
-			$paramters['partnerKey'] = $this->options['partner_key'];
+		if (isset($this->options['partner_key']) && $this->options['partner_key']) {
+			$parameters['partnerKey'] = $this->options['partner_key'];
 		}
 
 		$url .= (false === strpos($url, '?') ? '?' : '&');
